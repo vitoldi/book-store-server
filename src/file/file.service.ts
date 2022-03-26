2cc31bc1d436ec6f6357ad2dac276c454956a2c9
Vitali Logvin <vitali.logvin@noogadev.com>
2022-03-26 10:25:06 +0300

    image file path

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -13,12 +13,12 @@ export class FileService {
         try {
             const fileExtension = file.originalname.split('.').pop()
             const fileName = `${uuid.v4()}.${fileExtension}`
-            const filePath = path.resolve(__dirname, '..', 'static')
+            const filePath = path.resolve(__dirname, '..', 'static', type)
             if (!fs.existsSync(filePath)) {
                 fs.mkdirSync(filePath, {recursive: true})
             }
             fs.writeFileSync(path.resolve(filePath, fileName), file.buffer)
-            return `${fileName}`
+            return `${type}/${fileName}`
         } catch (error) {
             throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
